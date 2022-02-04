c5689fac4667c398004b1054c3a5313b84d9a52b
Prado789 <michalfiedler01@wp.pl>
2022-02-04 17:36:16 +0100

    Names

---
+++ Resources/GameObjects/Maps/Ship_Parts.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Ship_Parts" tilewidth="16" tileheight="16" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../../Textures/Mapa/Grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../../Textures/Mapa/Meadow.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../../Textures/Mapa/Tiles.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../../Textures/Mapa/Wall.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="../../Textures/Mapa/Wall_Green.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../../Textures/Mapa/Water.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../../Textures/Mapa/Wood.png"/>
+ </tile>
+</tileset>
