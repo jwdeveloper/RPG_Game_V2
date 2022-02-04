0af67c459bdb05ceff6658b9910bbe7a36ec9695
Prado789 <michalfiedler01@wp.pl>
2022-02-04 19:29:27 +0100

    Names

--- Resources/GameObjects/Maps/Ship_Parts.tsx
+++ Resources/GameObjects/Maps/Ship_Parts.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Ship_Parts" tilewidth="16" tileheight="16" tilecount="7" columns="0">
+<tileset version="1.5" tiledversion="1.7.2" name="Ship_Parts" tilewidth="16" tileheight="16" tilecount="22" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="16" height="16" source="../../Textures/Mapa/Grass.png"/>
@@ -21,5 +21,50 @@
  </tile>
  <tile id="6">
   <image width="16" height="16" source="../../Textures/Mapa/Wood.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="../../../../../Downloads/Wood_Barrier.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../../../../../Downloads/Wood_Barrier_Top.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_Corner.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_Corner_NoBG.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_croner_NoBG1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_Corner_BG.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_Corner_BG_Full.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_Corner_BG_Right.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_Corner_NoBG_Up.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_Corner_NoBG_Up_Down.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_Corner_NoBG_Left_UP.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_Corner_NoBG_Right_Up.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_Corner_BG_Left_Up.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="../../../../../Downloads/Barrier_Corner_BG_Right.png"/>
  </tile>
 </tileset>
